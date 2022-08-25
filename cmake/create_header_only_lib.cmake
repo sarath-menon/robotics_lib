01b867320243346ce5f174c876ab5555b69ab842
sarath-menon <sarathmenon.downey@gmail.com>
2022-08-26 00:05:47 +0530

    added func to create header only lib

---
+++ cmake/create_header_only_lib.cmake
@@ -0,0 +1,38 @@
+# to create a static library in cmake
+
+function(create_header_only_lib)
+
+    # ----------------------------------------------------------
+    # step 1: parse cmake arguments
+    # ----------------------------------------------------------
+
+    set(options VERBOSE)
+    set(oneValueArgs NAME)
+    set(multiValueArgs INCLUDE_DIRS LIB_DEP)
+    cmake_parse_arguments(LIB "${options}" "${oneValueArgs}"
+                        "${multiValueArgs}" ${ARGN} )
+
+    if(${LIB_VERBOSE})
+    message("header only lib name: " ${LIB_NAME})
+    message("include dirs: " ${LIB_INCLUDE_DIRS})
+    message("lib deps: " ${LIB_DEP})
+    endif()
+
+    # ----------------------------------------------------------
+    # step 2: create header only library
+    # ----------------------------------------------------------
+
+    add_library(${LIB_NAME} INTERFACE)
+    target_include_directories(${LIB_NAME} INTERFACE ${LIB_INCLUDE_DIRS})
+    target_link_libraries(${LIB_NAME} INTERFACE ${LIB_DEP})
+
+    if(VERBOSE)
+        message("added header only lib: " ${LIB_NAME})
+    endif()
+
+
+endfunction(create_header_only_lib)
+
+
+
+
--- cmake/top.cmake
+++ cmake/top.cmake
@@ -1,2 +1,3 @@
 include(${RL_PATH}/cmake/create_interface_lib.cmake)
 include(${RL_PATH}/cmake/create_static_lib.cmake)
+include(${RL_PATH}/cmake/create_header_only_lib.cmake)
