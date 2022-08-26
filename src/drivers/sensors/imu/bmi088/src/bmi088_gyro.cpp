513d200721c8addf7e888c2fc6330edca22bb13d
sarath-menon <sarathmenon.downey@gmail.com>
2022-08-26 17:42:25 +0530

    fixed accidental chane of var  in constructor

--- src/drivers/sensors/imu/bmi088/src/bmi088_gyro.cpp
+++ src/drivers/sensors/imu/bmi088/src/bmi088_gyro.cpp
@@ -6,7 +6,7 @@
 LOG_MODULE_DECLARE(cf_app);
 
 BMI088_Gyro::BMI088_Gyro(const struct device *i2c_dev)
-    : IMU{i2c_dev, ACC_CHIP_ADDR}, i2c_dev{i2c_dev, ACC_CHIP_ADDR} {
+    : IMU{i2c_dev, GYRO_CHIP_ADDR}, i2c_dev{i2c_dev, GYRO_CHIP_ADDR} {
   rl::err status = this->initialize();
   if (status == 0) {
     LOG_INF("mpu6050 imu gyro initialized");
